c0d737e5e78bfef92d857f72bc179d1c5e4c4da5
LUIS DANIEL RODRIGUEZ <60210187+luisdanielrs@users.noreply.github.com>
2020-03-11 18:48:11 -0600

    Add files via upload

---
+++ Ejercicio_01.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+    using namespace std;
+
+
+int main(){
+    int variableLocal = 5;
+    int * apLocal = &variableLocal;
+    int * apHeap = new int;
+
+    * apHeap = 7;
+    cout<<"Variable Local: " << variableLocal << endl;
+    cout<< "*apLocal: " << *apLocal << endl;
+
+    cout<< "*apHeap: " << *apHeap <<endl;
+    delete apHeap;
+    apHeap = new int;
+    *apHeap = 9;
+    cout<< "*apHeap: " <<*apHeap <<endl;
+    delete apHeap;
+
+return 0;
+}
---
+++ Ejercicio_02.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+    class GatoSimple{
+    public:
+        GatoSimple();
+        ~GatoSimple();
+
+    private:
+        int suEdad;
+
+    };
+
+
+    GatoSimple::GatoSimple(){
+        cout<<"Se llamo al constructor"<<endl;
+        suEdad = 1;
+    }
+
+    GatoSimple::~GatoSimple(){
+        cout<<"Se llamo al destructor"<<endl;
+    }
+
+
+int main(){
+    cout<<"GatoSimple Pelusa...."<<endl;
+    GatoSimple Pelusa;
+    cout<<"Gato simple *apFelix = new GatoSimple"<<endl;
+    GatoSimple * apFelix = new GatoSimple;
+    cout<<"delete apFelix...."<<endl;
+    delete apFelix;
+    cout<<"Saliendo, observe como se va Pelusa..."<<endl;
+
+
+
+return 0;
+}
